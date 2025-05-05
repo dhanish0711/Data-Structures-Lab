6c711ccd7aaf12d4d4c2237bdfbf287e48967d7b
dhanish0711 <dhanishladwani@gmail.com>
2025-05-05 17:20:49 +0530

    1.1.C

--- Experiment 1/1.1.C
+++ Experiment 1/1.1.C
@@ -1,3 +1,5 @@
+// Write a program to search a key element with in the given array of elements using linear search process.
+
 #include<stdio.h>
 void main() {
 	int a[20], i, n, key, flag = 0, pos;
